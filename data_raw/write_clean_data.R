eaa6d5a55b299fa3715acaa5036af8f580797585
Victoria Lynch <victorialynch@victorias-air-8.dyn.nyp.org>
2023-07-26 13:30:56 -0400

    update state dataset

--- data_raw/write_clean_data.R
+++ data_raw/write_clean_data.R
@@ -1,7 +1,25 @@
 library(tidyverse)
 library(usethis)
 library(devtools)
+library(MMWRweek)
 
 nndss_water <- read_csv("data_raw/health_state_data.csv")
 
-use_data(nndss_water, overwrite = TRUE)
+state_nndss_data <- nndss_water %>%
+  mutate(pathogen = case_when(
+    pathogen == 'stec' ~ 'ecoli_infection',
+    pathogen == 'lgn' ~ 'legionnaires_disease',
+    pathogen == 'crypto' ~ 'cryptosporidiosis',
+    pathogen == 'salm' ~ 'salmonellosis',
+    pathogen == 'ecoli' ~ 'ecoli_infection',
+    pathogen == 'shig' ~ 'shigellosis',
+    pathogen == 'grd' ~ 'giardiasis'
+  )) %>%
+  mutate(date = MMWRweek2Date(year, week)) %>%
+  rename(
+    annual_cumulative_cases = num_cases,
+    current_week_cases = current_week
+  )
+
+use_data(state_nndss_data, overwrite = TRUE)
+
